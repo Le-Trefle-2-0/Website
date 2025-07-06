0799d6dceacd48839ec2f6b36180f87d2700c2e3
PaulPR-44 <paul@peronredon.eu>
2025-07-06 16:14:12 +0200

    base navbar

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    output: 'export'
+    output: 'export',
+    devIndicators: false
 };
 
 export default nextConfig;
